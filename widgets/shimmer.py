2aa22eda3fcd6a62bff45bb4a9c6c3f1fe01b1ef
Deven Lahoti <deven.lahoti@gmail.com>
2016-12-11 06:55:56 -0500

    add adjustable sat and val

--- widgets/shimmer.py
+++ widgets/shimmer.py
@@ -3,7 +3,7 @@ from tepwall.widget import Widget
 from numpy import sin, cos, square, linspace
 
 class Shimmer(Widget):
-    def __init__(self, a=1.5, b=22, c=60, d=.522222, e=.71):
+    def __init__(self, a=1.5, b=22, c=60, d=.522222, e=.71, saturation=255, brightness=255):
         """
         a ~ speed of shape change
         b ~ speed of hue change
@@ -11,7 +11,7 @@ class Shimmer(Widget):
         d ~ x velocity of center
         e ~ y velocity of center
         """
-        self.consts = a, b, c, d, e
+        self.consts = a, b, c, d, e, saturation, brightness
     def load(self, panel):
         self.i = (linspace(-1, 1, panel.shape[1]),
                   linspace(-1, 1, panel.shape[0])[:,None])
@@ -19,6 +19,7 @@ class Shimmer(Widget):
         (a, b, c, d, e), (x, y), t = self.consts, self.i, time.time()
         panel[...,0] = (c*cos(a*t)*square(x-cos(d*t))+b*t+
                         c*sin(a*t)*square(y-sin(e*t)))%180
-        panel[...,1:] = 255
+        panel[...,1] = saturation
+        panel[...,2:] = brightness
         cv2.cvtColor(panel, cv2.COLOR_HSV2RGB, panel)
 
